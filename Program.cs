f066311804ea60ba91b1786d3a2cc2770b6dc073
Gyokujin <dd43454548@naver.com>
2024-07-09 01:25:29 +0900

    초기 커밋.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DotNet_Framework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
